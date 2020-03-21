b042528a703becf125e7dcd744fc071c2cde1ec5
János Gönye <janos.gonye@gmail.com>
2020-03-21 21:24:16 +0100

    fix | do not convert SECRET KEY envvar to bool

--- pass_manager/settings.py
+++ pass_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = bool(os.getenv("SECRET_KEY", "secret_key123"))
+SECRET_KEY = os.getenv("SECRET_KEY", "secret_key123")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG", 0))
