4379b96c07b0aa04917f92f7bb67b3fed8a6567a
János Gönye <janos.gonye@gmail.com>
2020-03-21 20:23:43 +0100

    configure rest framework to use jwt auth

--- pass_manager/settings.py
+++ pass_manager/settings.py
@@ -133,3 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
