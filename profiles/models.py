6395efa9150c71cb31ee9fbf50fa35713119c215
János Gönye <janos.gonye@gmail.com>
2020-03-21 21:56:59 +0100

    add 'EncryptedProfile' model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class EncryptedProfile(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True)
+    data = models.TextField()
+    history = HistoricalRecords()
