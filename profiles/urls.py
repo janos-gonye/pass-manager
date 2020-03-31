0b826e6a2078880ebbde6e40e989870f9bd768ac
János Gönye <janos.gonye@gmail.com>
2020-03-31 23:30:35 +0200

    remove 'EncryptedProfileViewset'

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,1 @@
-from rest_framework import routers
-
-from profiles.views import EncryptedProfileViewSet
-
-router = routers.SimpleRouter()
-router.register('', EncryptedProfileViewSet, basename='profiles')
-
-urlpatterns = router.urls
+urlpatterns = []
