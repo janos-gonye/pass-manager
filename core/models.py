3b517d4bcc7456d5d7a81e070e1cebef937ddbf1
János Gönye <janos.gonye@gmail.com>
2020-03-31 23:18:57 +0200

    create encrypted profile on user creation

--- core/models.py
+++ core/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from simple_history.models import HistoricalRecords
 
 
 class User(AbstractUser):
     history = HistoricalRecords()
+
+
+@receiver(post_save, sender=User)
+def create_user_encrypted_profile_on_user_created(sender, instance,
+                                                  created, **kwargs):
+    from profiles.models import EncryptedProfile
+    if not created:
+        return
+    EncryptedProfile.objects.create(user=instance, data='')
