88829bd027d76813f5414cba8619bda3c7fddc1d
János Gönye <janos.gonye@gmail.com>
2020-04-04 14:57:38 +0200

    add whitenoise to server staticfiles

--- pass_manager/settings.py
+++ pass_manager/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
