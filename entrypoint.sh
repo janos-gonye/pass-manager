a3e9719f6ebd0b036b390ec6aaca3ed1293debf2
János Gönye <janos.gonye@gmail.com>
2020-04-05 14:31:14 +0200

    fix entrypoint files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+echo "Waiting for postgres..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
 
 # To avoid to enter the container frequently
 python manage.py migrate
